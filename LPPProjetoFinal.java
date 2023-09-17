c037fde417f60b491eb9c9bbe1b8d31e07bc1811
João Araújo <73491681+Jony-17@users.noreply.github.com>
2023-09-17 19:46:58 +0100

    Update LPPProjetoFinal.java

--- LPPProjetoFinal.java
+++ LPPProjetoFinal.java
@@ -22,32 +22,5 @@ public class LPPProjetoFinal extends JFrame {
     public static void main(String[] args) {
 
         LPPProjetoFinal obj = new LPPProjetoFinal();
-
-        //ArrayList<Funcionarios> listaFuncionarios = new ArrayList<>();
-        //listaFuncionarios.add(new Funcionarios("Teste1", 1, 1, "Teste1"));
-        //listaFuncionarios.add(new Funcionarios("Teste2", 2, 2, "Teste2"));
-        //listaFuncionarios.add(new Funcionarios("Teste3", 3, 3, "Teste3"));
-
-        //System.out.println(listaFuncionarios);
-
-        /*try {
-            ObjectOutputStream cout = new ObjectOutputStream(
-                    new FileOutputStream("dados.txt"));
-            cout.writeObject(listaFuncionarios);
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-        ArrayList<Funcionarios> listaFuncionarios2 = new ArrayList<>();
-
-        try {
-            ObjectInputStream cin = new ObjectInputStream(
-                    new FileInputStream("dados.txt"));
-            listaFuncionarios2 = (ArrayList) cin.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        //System.out.println(listaFuncionarios2);*/
     }
 }
